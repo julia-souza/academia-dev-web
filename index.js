5030ec903c1b42116fd1c5906a6910797974b28e
Júlia Souza <juliadsouzaborges@gmail.com>
2022-11-07 18:47:28 -0300

    aluno router

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDatabase from "./config/db.js";
+import alunoRoutes from "./routes/alunoRoutes.js";
 
 const app = express();
 dotenv.config();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/api/alunos", alunoRoutes);
+
+
 app.listen(8080, () => {
   connectDatabase();
 //   console.log("Servidor rodando na porta 8080.");
 });
+
